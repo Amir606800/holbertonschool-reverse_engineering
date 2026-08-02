49aacc286cf152352b12ef57a336737c2b26db2b
Amir Asadli <asadliamir800@gmail.com>
2026-08-02 15:59:43 +0400

    Changes Saved

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -21,7 +21,7 @@ if ! file "$file_name" | grep -q "ELF"; then
 fi
 
 # Extract ELF header information
-magic_number=$(readelf -h "$file_name" | grep "Magic:" | sed 's/^[[:space:]]*Magic:[[:space:]]*//')
+magic_number=$(readelf -h "$file_name" | grep "Magic:" | sed 's/^[[:space:]]*Magic:[[:space:]]*$//')
 class=$(readelf -h "$file_name" | grep "Class:" | sed 's/^[[:space:]]*Class:[[:space:]]*//')
 byte_order=$(readelf -h "$file_name" | grep "Data:" | awk -F', ' '{print $2}')
 entry_point_address=$(readelf -h "$file_name" | grep "Entry point address:" | sed 's/^[[:space:]]*Entry point address:[[:space:]]*//')
